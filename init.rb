18b70106515c5bf988f5635bb86821f2fee486fa
James Le Cuirot <james.le-cuirot@yakara.com>
2012-06-28 14:23:42 +0100

    Rearrange for use as a gem with Rails 3. Bump to version 0.2.0.

--- init.rb
+++
@@ -1,4 +0,0 @@
-require 'mc_money'
-require 'money_column'
-
-ActiveRecord::Base.send :include, MoneyColumn
--- lib/money_column.rb
+++ lib/money_column.rb
@@ -1,36 +1,4 @@
-module MoneyColumn
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
+require File.expand_path('../mc_money', __FILE__)
+require File.expand_path('../money_column/money_column', __FILE__)
 
-  module ClassMethods
-
-    def money_column(*columns)
-      options = columns.extract_options! || {}
-
-      decimal_places = options[:decimal_places] || 2
-
-      [columns].flatten.each do |name|
-        define_method(name) do
-          value = read_attribute(name)
-          value.blank? ? nil : McMoney.new(read_attribute(name), options)
-        end
-
-        define_method("#{name}_before_type_cast") do
-          send(name) && sprintf("%.#{decimal_places}f", read_attribute(name))
-        end
-
-        define_method("#{name}=") do |value|
-          if value.blank?
-            write_attribute(name, nil)
-            nil
-          else
-            money = value.to_money(decimal_places)
-            write_attribute(name, money.value)
-            money
-          end
-        end
-      end
-    end
-  end
-end
+ActiveRecord::Base.send :include, MoneyColumn
